69c2de1e8572a962e63ba86b6c0e5e76ec7727c1
IbaiBuR <ibaibur@chessnibble.com>
2024-04-21 23:33:52 +0200

    Add basic types and enums

--- src/types.hpp
+++ src/types.hpp
@@ -7,3 +7,77 @@ using U16 = std::uint16_t;
 using U32 = std::uint32_t;
 using U64 = std::uint64_t;
 
+using I8  = std::int8_t;
+using I16 = std::int16_t;
+using I32 = std::int32_t;
+
+enum class File : U8 {
+    A_FILE,
+    B_FILE,
+    C_FILE,
+    D_FILE,
+    E_FILE,
+    F_FILE,
+    G_FILE,
+    H_FILE,
+    FILE_NB
+};
+
+enum class Rank : U8 {
+    RANK_1,
+    RANK_2,
+    RANK_3,
+    RANK_4,
+    RANK_5,
+    RANK_6,
+    RANK_7,
+    RANK_8,
+    RANK_NB
+};
+
+// clang-format off
+enum class Square : U8 {
+    A8, B8, C8, D8, E8, F8, G8, H8,
+    A7, B7, C7, D7, E7, F7, G7, H7,
+    A6, B6, C6, D6, E6, F6, G6, H6,
+    A5, B5, C5, D5, E5, F5, G5, H5,
+    A4, B4, C4, D4, E4, F4, G4, H4,
+    A3, B3, C3, D3, E3, F3, G3, H3,
+    A2, B2, C2, D2, E2, F2, G2, H2,
+    A1, B1, C1, D1, E1, F1, G1, H1,
+    SQUARE_NB, NO_SQ
+};
+// clang-format on
+
+enum class Color : U8 {
+    WHITE,
+    BLACK,
+    COLOR_NB
+};
+
+enum class PieceType : U8 {
+    PAWN,
+    KNIGHT,
+    BISHOP,
+    ROOK,
+    QUEEN,
+    KING,
+    PIECETYPE_NB
+};
+
+enum class Piece : I8 {
+    W_PAWN,
+    W_KNIGHT,
+    W_BISHOP,
+    W_ROOK,
+    W_QUEEN,
+    W_KING,
+    B_PAWN,
+    B_KNIGHT,
+    B_BISHOP,
+    B_ROOK,
+    B_QUEEN,
+    B_KING,
+    PIECE_NB,
+    NO_PIECE = -1
+};
