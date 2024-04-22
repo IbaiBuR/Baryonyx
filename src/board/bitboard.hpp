17da4333d6cf094d4e8fef39754daba47ae58965
IbaiBuR <ibaibur@chessnibble.com>
2024-04-22 20:19:20 +0200

    Refactor Bitboards::Util namespace and add shift function

--- src/board/bitboard.hpp
+++ src/board/bitboard.hpp
@@ -2,37 +2,7 @@
 
 #include "../types.hpp"
 
-
 namespace Board::Bitboards {
-
-namespace Util {
-
-constexpr U64 FileABB = 0x101010101010101ULL;
-constexpr U64 FileBBB = 0x202020202020202ULL;
-constexpr U64 FileCBB = 0x404040404040404ULL;
-constexpr U64 FileDBB = 0x808080808080808ULL;
-constexpr U64 FileEBB = 0x1010101010101010ULL;
-constexpr U64 FileFBB = 0x2020202020202020ULL;
-constexpr U64 FileGBB = 0x4040404040404040ULL;
-constexpr U64 FileHBB = 0x8080808080808080ULL;
-
-constexpr U64 Rank1BB = 0xFF00000000000000ULL;
-constexpr U64 Rank2BB = 0x00FF000000000000ULL;
-constexpr U64 Rank3BB = 0x0000FF0000000000ULL;
-constexpr U64 Rank4BB = 0x000000FF00000000ULL;
-constexpr U64 Rank5BB = 0x00000000FF000000ULL;
-constexpr U64 Rank6BB = 0x0000000000FF0000ULL;
-constexpr U64 Rank7BB = 0x000000000000FF00ULL;
-constexpr U64 Rank8BB = 0x00000000000000FFULL;
-
-constexpr File fileOf(const Square sq) { return static_cast<File>(static_cast<U8>(sq) & 7); }
-constexpr Rank rankOf(const Square sq) { return static_cast<Rank>(static_cast<U8>(sq) >> 3); }
-
-constexpr Square squareOf(const U8 file, const U8 rank) {
-    return static_cast<Square>((rank << 3) + file);
-}
-
-} // namespace Util
 
 class Bitboard {
     public:
@@ -66,6 +36,16 @@ class Bitboard {
             return Bitboard(data ^ other.data);
         }
 
+        constexpr Bitboard operator~() const { return Bitboard(~data); }
+
+        constexpr Bitboard operator<<(const Bitboard &other) const {
+            return Bitboard(data << other.data);
+        }
+
+        constexpr Bitboard operator>>(const Bitboard &other) const {
+            return Bitboard(data >> other.data);
+        }
+
         constexpr Bitboard operator&=(const Bitboard &other) {
             data &= other.data;
             return *this;
@@ -81,9 +61,86 @@ class Bitboard {
             return *this;
         }
 
+        constexpr Bitboard operator<<=(const Bitboard &other) {
+            data <<= other.data;
+            return *this;
+        }
+
+        constexpr Bitboard operator>>=(const Bitboard &other) {
+            data >>= other.data;
+            return *this;
+        }
+
+        constexpr Bitboard operator<<(const int shift) const { return Bitboard(data << shift); }
+        constexpr Bitboard operator>>(const int shift) const { return Bitboard(data >> shift); }
+
     private:
         U64 data;
 };
 
+namespace Util {
+
+constexpr auto FileABB = Bitboard(0x101010101010101ULL);
+constexpr auto FileBBB = Bitboard(0x202020202020202ULL);
+constexpr auto FileCBB = Bitboard(0x404040404040404ULL);
+constexpr auto FileDBB = Bitboard(0x808080808080808ULL);
+constexpr auto FileEBB = Bitboard(0x1010101010101010ULL);
+constexpr auto FileFBB = Bitboard(0x2020202020202020ULL);
+constexpr auto FileGBB = Bitboard(0x4040404040404040ULL);
+constexpr auto FileHBB = Bitboard(0x8080808080808080ULL);
+
+constexpr auto Rank1BB = Bitboard(0xFF00000000000000ULL);
+constexpr auto Rank2BB = Bitboard(0x00FF000000000000ULL);
+constexpr auto Rank3BB = Bitboard(0x0000FF0000000000ULL);
+constexpr auto Rank4BB = Bitboard(0x000000FF00000000ULL);
+constexpr auto Rank5BB = Bitboard(0x00000000FF000000ULL);
+constexpr auto Rank6BB = Bitboard(0x0000000000FF0000ULL);
+constexpr auto Rank7BB = Bitboard(0x000000000000FF00ULL);
+constexpr auto Rank8BB = Bitboard(0x00000000000000FFULL);
+
+constexpr File fileOf(const Square sq) { return static_cast<File>(static_cast<U8>(sq) & 7); }
+constexpr Rank rankOf(const Square sq) { return static_cast<Rank>(static_cast<U8>(sq) >> 3); }
+
+constexpr Square squareOf(const U8 file, const U8 rank) {
+    return static_cast<Square>((rank << 3) + file);
+}
+
+} // namespace Util
+
 void printBB(Bitboard bitboard);
+
+template <Direction d>
+constexpr Bitboard shift(const Bitboard &bb) {
+    switch (d)
+    {
+    case Direction::NORTH:
+        return Bitboard(bb >> 8);
+        break;
+    case Direction::SOUTH:
+        return Bitboard(bb << 8);
+        break;
+    case Direction::EAST:
+        return Bitboard(bb << 1) & ~Util::FileABB;
+        break;
+    case Direction::WEST:
+        return Bitboard(bb >> 1) & ~Util::FileHBB;
+        break;
+    case Direction::NORTH_EAST:
+        return Bitboard(bb >> 7) & ~Util::FileABB;
+        break;
+    case Direction::NORTH_WEST:
+        return Bitboard(bb >> 9) & ~Util::FileHBB;
+        break;
+    case Direction::SOUTH_EAST:
+        return Bitboard(bb << 9) & ~Util::FileABB;
+        break;
+    case Direction::SOUTH_WEST:
+        return Bitboard(bb << 7) & ~Util::FileHBB;
+        break;
+    default:
+        return Bitboard(0ULL);
+        break;
+    }
+}
+
 } // namespace Board::Bitboards
