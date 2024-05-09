cee16f8daa935ccd66342d4d47b8591b020ce259
IbaiBuR <ibaibur@chessnibble.com>
2024-05-09 14:59:12 +0200

    feat: Add definition of sliding attacks lookups

--- src/board/bitboard/attacks.cpp
+++ src/board/bitboard/attacks.cpp
@@ -1,3 +1,10 @@
 #include "attacks.hpp"
 
-namespace Board::Bitboards::Attacks {}
+namespace Board::Bitboards::Attacks {
+
+std::array<std::array<Bitboard, maxBishopBlockersConfig>, static_cast<U8>(Square::SQUARE_NB)>
+    bishopAttacks;
+std::array<std::array<Bitboard, maxRookBlockersConfig>, static_cast<U8>(Square::SQUARE_NB)>
+    rookAttacks;
+
+} // namespace Board::Bitboards::Attacks
