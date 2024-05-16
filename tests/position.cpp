350e66162871860e6a0d7c1c120aa4f56e768668
IbaiBuR <ibaibur@chessnibble.com>
2024-05-16 18:32:35 +0200

    test: Add basic position test from startpos

---
+++ tests/position.cpp
@@ -0,0 +1,25 @@
+#include "../src/board/position.hpp"
+#include "doctest/doctest.hpp"
+
+using namespace Board;
+
+TEST_SUITE("Positiion Tests") {
+    TEST_CASE("fen parsing") {
+        SUBCASE("start position") {
+            Position pos(Util::startPosFen);
+
+            CHECK(pos.pieceTypeBB(PieceType::PAWN) == Bitboards::Bitboard(0xFF00000000FF00ULL));
+            CHECK(pos.pieceTypeBB(PieceType::KNIGHT) == Bitboards::Bitboard(0x4200000000000042ULL));
+            CHECK(pos.pieceTypeBB(PieceType::KING) == Bitboards::Bitboard(0x1000000000000010ULL));
+            CHECK(pos.pieceTypeBB(PieceType::BISHOP) == Bitboards::Bitboard(0x2400000000000024ULL));
+            CHECK(pos.pieceTypeBB(PieceType::ROOK) == Bitboards::Bitboard(0x8100000000000081ULL));
+            CHECK(pos.pieceTypeBB(PieceType::QUEEN) == Bitboards::Bitboard(0x800000000000008ULL));
+
+            CHECK(pos.sideToMove() == Color::WHITE);
+            CHECK(pos.castlingRights() == CastlingRights(CastlingRights::Flags::ALL));
+            CHECK(pos.epSquare() == Square::NO_SQ);
+            CHECK(pos.fiftyMoveRule() == 0);
+            CHECK(pos.fullMoves() == 1);
+        }
+    }
+}
