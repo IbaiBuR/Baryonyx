b2b14ff1ae4451159e74b33b2a8d6b4fe51951a2
IbaiBuR <ibaibur@chessnibble.com>
2024-05-21 01:41:27 +0200

    feat: Add MoveList class to prepare for move generation

---
+++ src/moves/movelist.cpp
@@ -0,0 +1,13 @@
+#include "movelist.hpp"
+
+#include <print>
+
+namespace Moves {
+
+void printMoveList(const MoveList &moveList) {
+    for (U32 i = 0; i < moveList.size(); i++)
+        std::println("{0:3}. Move: {1:<5} Score {2:<5}", i + 1, moveList.moveAt(i).toString(),
+                     moveList.scoreAt(i));
+}
+
+} // namespace Moves
---
+++ src/moves/movelist.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <array>
+#include <cassert>
+
+#include "move.hpp"
+
+namespace Moves {
+
+constexpr int maxMoves = 256;
+
+struct ScoredMove {
+        Score score;
+        Move  move;
+};
+
+class MoveList {
+    public:
+        void push(const Move &move) {
+            assert(size + 1 < maxMoves);
+            moves[count++].move = move;
+        }
+
+        void clear() { count = 0; }
+
+        [[nodiscard]] U32 size() const { return count; }
+
+        [[nodiscard]] Move moveAt(const U32 index) const {
+            assert(index < maxMoves);
+            return moves[index].move;
+        }
+
+        [[nodiscard]] Score scoreAt(const U32 index) const {
+            assert(index < maxMoves);
+            return moves[index].score;
+        }
+
+    private:
+        std::array<ScoredMove, maxMoves> moves{};
+        U32                              count{};
+};
+
+void printMoveList(const MoveList &moveList);
+
+} // namespace Moves
