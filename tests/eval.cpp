67477634c5208e3c4f53aecad78310f61aca7726
IbaiBuR <ibaibur@chessnibble.com>
2024-06-02 02:20:59 +0200

    test: Add initial evaluation tests

---
+++ tests/eval.cpp
@@ -0,0 +1,22 @@
+#include "../src/eval/eval.hpp"
+#include "doctest/doctest.hpp"
+
+using namespace Eval;
+
+TEST_SUITE("Eval Tests") {
+    TEST_CASE("packed score") {
+        SUBCASE("midgame") {
+            CHECK(PSQT::pawnTable[0].midgame() == 0);
+            CHECK(PSQT::pawnTable[63].midgame() == 0);
+            CHECK(PSQT::knightTable[0].midgame() == -167);
+            CHECK(PSQT::knightTable[63].midgame() == -23);
+        }
+
+        SUBCASE("endgame") {
+            CHECK(PSQT::pawnTable[0].endgame() == 0);
+            CHECK(PSQT::pawnTable[63].endgame() == 0);
+            CHECK(PSQT::knightTable[0].endgame() == -58);
+            CHECK(PSQT::knightTable[63].endgame() == -64);
+        }
+    }
+}
