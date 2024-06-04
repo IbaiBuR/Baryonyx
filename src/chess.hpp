8d438d6e679e73c5e88d5450e59a9b81f5a3d851
IbaiBuR <ibaibur@chessnibble.com>
2024-06-05 00:21:50 +0200

    refactor: Add type specification for Square enum class

--- src/chess.hpp
+++ src/chess.hpp
@@ -27,7 +27,7 @@ enum class Rank : u8 {
 };
 
 // clang-format off
-enum class Square {
+enum class Square : u8 {
     A1, B1, C1, D1, E1, F1, G1, H1,
     A2, B2, C2, D2, E2, F2, G2, H2,
     A3, B3, C3, D3, E3, F3, G3, H3,
