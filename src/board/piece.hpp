910448bddfdb4bb5741f330623d89cabaef905d0
IbaiBuR <ibaibur@chessnibble.com>
2024-06-20 00:04:59 +0200

    refactor: Use switches instead of array lookups for piece utils
    BENCH: 55892630

--- src/board/piece.hpp
+++ src/board/piece.hpp
@@ -1,36 +1,115 @@
 #pragma once
 
-#include <array>
-#include <unordered_map>
-
 #include "../chess.hpp"
 
 namespace Board::Pieces {
 
-constexpr std::array pieceToChar = {'P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k'};
+constexpr char pieceToChar(const Piece p) {
+    switch (p) {
+    case Piece::W_PAWN:
+        return 'P';
+    case Piece::W_KNIGHT:
+        return 'N';
+    case Piece::W_BISHOP:
+        return 'B';
+    case Piece::W_ROOK:
+        return 'R';
+    case Piece::W_QUEEN:
+        return 'Q';
+    case Piece::W_KING:
+        return 'K';
+    case Piece::B_PAWN:
+        return 'p';
+    case Piece::B_KNIGHT:
+        return 'n';
+    case Piece::B_BISHOP:
+        return 'b';
+    case Piece::B_ROOK:
+        return 'r';
+    case Piece::B_QUEEN:
+        return 'q';
+    case Piece::B_KING:
+        return 'k';
+    default:
+        return ' ';
+    }
+}
 
-const std::unordered_map<char, Piece> charToPiece = {
-    {'P',   Piece::W_PAWN},
-    {'N', Piece::W_KNIGHT},
-    {'B', Piece::W_BISHOP},
-    {'R',   Piece::W_ROOK},
-    {'Q',  Piece::W_QUEEN},
-    {'K',   Piece::W_KING},
-    {'p',   Piece::B_PAWN},
-    {'n', Piece::B_KNIGHT},
-    {'b', Piece::B_BISHOP},
-    {'r',   Piece::B_ROOK},
-    {'q',  Piece::B_QUEEN},
-    {'k',   Piece::B_KING}
-};
+constexpr Piece charToPiece(const char c) {
+    switch (c) {
+    case 'P':
+        return Piece::W_PAWN;
+    case 'N':
+        return Piece::W_KNIGHT;
+    case 'B':
+        return Piece::W_BISHOP;
+    case 'R':
+        return Piece::W_ROOK;
+    case 'Q':
+        return Piece::W_QUEEN;
+    case 'K':
+        return Piece::W_KING;
+    case 'p':
+        return Piece::B_PAWN;
+    case 'n':
+        return Piece::B_KNIGHT;
+    case 'b':
+        return Piece::B_BISHOP;
+    case 'r':
+        return Piece::B_ROOK;
+    case 'q':
+        return Piece::B_QUEEN;
+    case 'k':
+        return Piece::B_KING;
+    default:
+        return Piece::NONE;
+    }
+}
 
-constexpr std::array pieceToPieceType = {PieceType::PAWN, PieceType::KNIGHT, PieceType::BISHOP,
-                                         PieceType::ROOK, PieceType::QUEEN,  PieceType::KING,
-                                         PieceType::PAWN, PieceType::KNIGHT, PieceType::BISHOP,
-                                         PieceType::ROOK, PieceType::QUEEN,  PieceType::KING};
+constexpr PieceType pieceToPieceType(const Piece p) {
+    switch (p) {
+    case Piece::W_PAWN:
+    case Piece::B_PAWN:
+        return PieceType::PAWN;
+    case Piece::W_KNIGHT:
+    case Piece::B_KNIGHT:
+        return PieceType::KNIGHT;
+    case Piece::W_BISHOP:
+    case Piece::B_BISHOP:
+        return PieceType::BISHOP;
+    case Piece::W_ROOK:
+    case Piece::B_ROOK:
+        return PieceType::ROOK;
+    case Piece::W_QUEEN:
+    case Piece::B_QUEEN:
+        return PieceType::QUEEN;
+    case Piece::W_KING:
+    case Piece::B_KING:
+        return PieceType::KING;
+    default:
+        return PieceType::NONE;
+    }
+}
 
-constexpr std::array pieceColor = {Color::WHITE, Color::WHITE, Color::WHITE, Color::WHITE,
-                                   Color::WHITE, Color::WHITE, Color::BLACK, Color::BLACK,
-                                   Color::BLACK, Color::BLACK, Color::BLACK, Color::BLACK};
+constexpr Color pieceColor(const Piece p) {
+    switch (p) {
+    case Piece::W_PAWN:
+    case Piece::W_KNIGHT:
+    case Piece::W_BISHOP:
+    case Piece::W_ROOK:
+    case Piece::W_QUEEN:
+    case Piece::W_KING:
+        return Color::WHITE;
+    case Piece::B_PAWN:
+    case Piece::B_KNIGHT:
+    case Piece::B_BISHOP:
+    case Piece::B_ROOK:
+    case Piece::B_QUEEN:
+    case Piece::B_KING:
+        return Color::BLACK;
+    default:
+        return Color::NONE;
+    }
+}
 
 } // namespace Board::Pieces
