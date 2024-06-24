e01d27886abf3528401861ca859a0f395400b895
IbaiBuR <ibaibur@chessnibble.com>
2024-06-24 20:01:33 +0200

    test: Add Zobrist Hashing tests
    BENCH: 55892630

--- tests/position.cpp
+++ tests/position.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 
 using namespace board;
+using namespace moves;
 
 TEST_SUITE("Position Tests") {
     TEST_CASE("fen parsing") {
@@ -11,11 +12,16 @@ TEST_SUITE("Position Tests") {
             position pos(util::start_pos_fen);
 
             CHECK(pos.piece_type_bb(piece_type::pawn) == bitboards::bitboard(0xFF00000000FF00ULL));
-            CHECK(pos.piece_type_bb(piece_type::knight) == bitboards::bitboard(0x4200000000000042ULL));
-            CHECK(pos.piece_type_bb(piece_type::king) == bitboards::bitboard(0x1000000000000010ULL));
-            CHECK(pos.piece_type_bb(piece_type::bishop) == bitboards::bitboard(0x2400000000000024ULL));
-            CHECK(pos.piece_type_bb(piece_type::rook) == bitboards::bitboard(0x8100000000000081ULL));
-            CHECK(pos.piece_type_bb(piece_type::queen) == bitboards::bitboard(0x800000000000008ULL));
+            CHECK(pos.piece_type_bb(piece_type::knight)
+                  == bitboards::bitboard(0x4200000000000042ULL));
+            CHECK(pos.piece_type_bb(piece_type::king)
+                  == bitboards::bitboard(0x1000000000000010ULL));
+            CHECK(pos.piece_type_bb(piece_type::bishop)
+                  == bitboards::bitboard(0x2400000000000024ULL));
+            CHECK(pos.piece_type_bb(piece_type::rook)
+                  == bitboards::bitboard(0x8100000000000081ULL));
+            CHECK(pos.piece_type_bb(piece_type::queen)
+                  == bitboards::bitboard(0x800000000000008ULL));
 
             CHECK(pos.side_to_move() == color::white);
             CHECK(pos.castling() == castling_rights(castling_rights::castling_flag::all));
@@ -64,6 +70,37 @@ TEST_SUITE("Position Tests") {
             CHECK(position("r2q1knr/1bp3bp/p1n3p1/1p1pPp2/1P2P3/P1P2NP1/3N1PBP/R1BQK1R1 b Q - 0 11")
                       .to_fen()
                   == "r2q1knr/1bp3bp/p1n3p1/1p1pPp2/1P2P3/P1P2NP1/3N1PBP/R1BQK1R1 b Q - 0 11");
+        }
+    }
+
+    TEST_CASE("zobrist hashing") {
+        SUBCASE("start position") {
+            CHECK(position(util::start_pos_fen).key() == 0x63FB272195DEE353ULL);
+        }
+
+        SUBCASE("after double push") {
+            position pos(util::start_pos_fen);
+            pos.make_move(move(square::e2, square::e4, move::move_flag::double_push));
+
+            CHECK(pos.key()
+                  == position("rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq e3 0 1").key());
+        }
+
+        SUBCASE("after castling") {
+            position pos("rnbqk2r/pppp1ppp/5n2/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 4 4");
+            pos.make_move(move(square::e1, square::g1, move::move_flag::castle));
+
+            CHECK(pos.key()
+                  == position("rnbqk2r/pppp1ppp/5n2/2b1p3/2B1P3/5N2/PPPP1PPP/RNBQ1RK1 b kq - 5 4")
+                         .key());
+        }
+
+        SUBCASE("after en passant") {
+            position pos("rnbqkbnr/ppp2ppp/4p3/3pP3/8/8/PPPP1PPP/RNBQKBNR w KQkq d6 0 3");
+            pos.make_move(move(square::e5, square::d6, move::move_flag::en_passant));
+
+            CHECK(pos.key()
+                  == position("rnbqkbnr/ppp2ppp/3Pp3/8/8/8/PPPP1PPP/RNBQKBNR b KQkq - 0 3").key());
         }
     }
 }
