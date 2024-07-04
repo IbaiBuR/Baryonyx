78560c8ac8cbb2946e66d3a8fec4403f0ca3b7dd
IbaiBuR <ibaibur@chessnibble.com>
2024-07-04 20:11:58 +0200

    feat: Add new type aliases and constants
    BENCH: 20354571

--- src/types.hpp
+++ src/types.hpp
@@ -7,6 +7,7 @@ using u8  = std::uint8_t;
 using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
+using u128 = __uint128_t;
 
 using usize = std::size_t;
 
@@ -16,3 +17,4 @@ using i32 = std::int32_t;
 
 using score       = i32;
 using zobrist_key = u64;
+using tt_key      = u16;
