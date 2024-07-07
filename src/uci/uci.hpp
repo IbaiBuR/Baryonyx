342b6b6077f2e4b8671bd57a08ede2297d14aac2
IbaiBuR <ibaibur@chessnibble.com>
2024-07-07 18:52:21 +0200

    chore: Bump version to 0.1.10
    BENCH: 24309923

--- src/uci/uci.hpp
+++ src/uci/uci.hpp
@@ -15,7 +15,7 @@ class command_handler {
     private:
         static constexpr auto author  = "Ibai Burgos";
         static constexpr auto name    = "Baryonyx";
-        static constexpr auto version = "0.1.6";
+        static constexpr auto version = "0.1.10";
 
         search::searcher m_searcher;
 
