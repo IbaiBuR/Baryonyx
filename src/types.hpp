e25d2644019b64ac23f7f1eef5f88ac43f868d35
IbaiBuR <ibaibur@chessnibble.com>
2024-09-14 12:21:21 +0200

    feat: Add fixed width floating point types
    BENCH: 109712035

--- src/types.hpp
+++ src/types.hpp
@@ -2,6 +2,7 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <stdfloat>
 
 using u8   = std::uint8_t;
 using u16  = std::uint16_t;
@@ -14,6 +15,10 @@ using usize = std::size_t;
 using i8  = std::int8_t;
 using i16 = std::int16_t;
 using i32 = std::int32_t;
+
+using f16 = std::float16_t;
+using f32 = std::float32_t;
+using f64 = std::float64_t;
 
 using score       = i32;
 using zobrist_key = u64;
