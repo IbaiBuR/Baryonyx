c781d2dcb2d0335958004bc3cc90136c7094df01
IbaiBuR <ibaibur@chessnibble.com>
2024-10-15 16:14:33 +0200

    refactor: Remove NumberInput concept
    BENCH: 21337504

--- src/utils/parsing.hpp
+++ src/utils/parsing.hpp
@@ -11,15 +11,12 @@ namespace utils::parsing {
 namespace internal {
 
 template <typename T>
-concept NumberInput = std::is_same_v<T, std::string> || std::is_same_v<T, std::string_view>;
-
-template <typename T>
 concept NumberOutput = std::is_integral_v<T> || std::is_floating_point_v<T>;
 
 } // namespace internal
 
-template <internal::NumberInput Input, internal::NumberOutput Output>
-constexpr std::optional<Output> to_number(const Input& s) {
+template <internal::NumberOutput Output>
+constexpr std::optional<Output> to_number(const std::string& s) {
     Output     value{};
     const auto result = std::from_chars(s.data(), s.data() + s.size(), value);
 
